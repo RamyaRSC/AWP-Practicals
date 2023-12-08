088c7d61c1461f952114b3604ad47b7c15eace29
Ramya Chidumalla <111015805+RamyaRSC@users.noreply.github.com>
2023-12-09 00:51:42 +0530

    Add files via upload

---
+++ DLL/DLL/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace DLL
+{
+    public class Class1
+    {
+        public double add(int x, int y)
+        {
+            return (x + y);
+        }
+        public double sub(int x, int y)
+        {
+            return (x - y);
+        }
+        public double mul(int x, int y)
+        {
+            return (x * y);
+        }
+
+    }
+}
